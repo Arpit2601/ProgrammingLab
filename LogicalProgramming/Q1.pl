2e081da26583d33291fa4ba9190798f8921fa7ae
ubuntu <arpit170101012@iitg.ac.in>
2020-10-26 14:35:55 +0530

    Assignment 2 complete

--- LogicalProgramming/Q1.pl
+++ LogicalProgramming/Q1.pl
@@ -16,18 +16,25 @@ female(shivkami).
 female(avantika).
 
 % Q1
-uncle(X, Y) :-  male(X),
-                parent(Z, Y),
-                parent(A, Z),
-                parent(A, X).
+% For X to be uncle of Y -> X has to be male and should be brother of one of Y's 
+% parents i.e. he should have a common parent with one of Y's parent
+
+uncle(X, Y) :-  
+    male(X),
+    parent(Z, Y),   % Z is a parent of Y 
+    parent(A, Z),   % Z and X have A as a common parent
+    parent(A, X).
 
 
 % Q2
-halfsister(X, Y) :- female(Y),
-                    parent(Z, X),
-                    parent(Z, Y),
-                    parent(A, X),
-                    parent(B, Y),
-                    A \== B,
-                    Z \== A,
-                    Z \== B.
+% For X to be Y's halfsister -> X has to be female and X and Y should have only 
+% one common parent
+halfsister(X, Y) :- 
+    female(Y),
+    parent(Z, X),   % Z is common parent of X and Y
+    parent(Z, Y),   
+    parent(A, X),   % X has A as other parent and Y has B as other parent
+    parent(B, Y),
+    A \== B,        % since we want only one common parent so Z, A and B are all different
+    Z \== A,
+    Z \== B.
